21ceb6f17ecc9d62167629cc53dc2d9f980c52f0
RafaelSilvaPereira <rafael.pereira@ccc.ufcg.edu.br>
2019-07-01 20:35:03 -0300

    documentando util.js

--- ucdb/controller/util/util.js
+++ ucdb/controller/util/util.js
@@ -1,3 +1,8 @@
-const isNumber = (n) => !isNaN(parseFloat(n)) && isFinite(n);
-
+/**
+ * @Author: Rafael da Silva Pereira Matricula: 117110921. UFCG: Ciência da Computação.
+ * Modulo util do sistema.
+ */
 export {isNumber}
+
+
+const isNumber = (n) => !isNaN(parseFloat(n)) && isFinite(n); // infelizmente depois do refatoramento o modulo fico simples demais
