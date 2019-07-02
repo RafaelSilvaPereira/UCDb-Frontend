406df99b1c078f256aacbdb14216fb4782969ed6
MagnerSA <magnersa99@gmail.com>
2019-07-02 00:07:23 -0300

    Tratando validacao de usuario

--- ucdb/view/struct/components/Register/register_features.js
+++ ucdb/view/struct/components/Register/register_features.js
@@ -1,3 +1,8 @@
+/**
+ * @author Marcelo Andrade - 117110910 / CC@UFCG
+ */
+
+
 import {UserModel} from "../../../../model/user.js";
 import {postData} from "../../../../controller/rest_controller.js";
 export {register_features}
@@ -16,18 +21,46 @@ function register_features(applicationHeader) {
 
     applicationHeader._form.submit.onclick = () => {
         let form = applicationHeader._form;
-        const user = new UserModel(
-            form.firstName.value,
-            form.lastName.value,
-            form.email.value,
-            form.psw.value
-        );
-        postData("students/", user)
-            .then(response => {
-                // console.log(response);
-                const message = `bem vindo ${response.firstName} ${response.secondName}`;
-                alert(message);
-                applicationHeader._closeButton.click();
-            }).catch(err => alert("usuario já cadastrado"));
+        let valid = true;
+
+        if (form.firstName.value === "") {
+            valid = false;
+            alert("Nome vazio invalido!");
+        } else if (form.lastName.value === "") {
+
+            valid = false;
+            alert("Sobrenome vazio invalido!");
+        } else if (form.email.value === "") {
+
+            valid = false;
+            alert("Email vazio invalido!");
+        } else if (!form.email.value.includes("@")) {
+
+            valid = false;
+            alert("Email invalido, ausencia de '@'!");
+        } else if (form.psw.value === "") {
+
+            valid = false;
+            alert("Senha vazia invalida!");
+        } else if (form.psw.value.length < 8) {
+
+            valid = false;
+            alert("Senha menor que oito caracteres!");
+        }
+
+        if (valid) {
+            const user = new UserModel(
+                form.firstName.value,
+                form.lastName.value,
+                form.email.value,
+                form.psw.value
+            );
+            postData("students/", user)
+                .then(response => {
+                    const message = `bem vindo ${response.firstName} ${response.secondName}`;
+                    alert(message);
+                    applicationHeader._closeButton.click();
+                }).catch(err => alert("usuario já cadastrado"));
+        }
     }
 }
